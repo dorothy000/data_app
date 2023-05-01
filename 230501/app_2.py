ffb6b0e17de202f293311cdffaf4b7624727184f
jooye <jooyeon.620@gmail.com>
2023-05-01 10:07:25 +0900

    230501

--- 230501/app_2.py
+++ 230501/app_2.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.header('display json')
-st.json([{'data':'name'}])
+st.json([{"data":"name"}])
+# JavaScript Object Notation은 Javascript 객체 문법으로 구조화된 데이터를 표현하기 위한 문자 기반의 표준 포맷
+# json에서는 큰따옴표 사용 권장
 
 st.header('display code')
 st.code('''
