2e870aef5f7ecdc6ca4b3cffdf8af11628858582
jooye <jooyeon.620@gmail.com>
2023-05-01 10:20:28 +0900

    230501

--- 230501/app_widget.py
+++ 230501/app_widget.py
@@ -1,1 +1,17 @@
-import
+import streamlit as st
+
+name = 'Jooyeon Kim'
+
+if st.button('Submit', key=1):
+    st.write('Name: {}'.format(name))
+
+if st.button('Submit', key=2):
+    st.write('Full Name: {}'.format(name))
+
+st.header('Radio Button')
+status = st.radio('What is your state','Active','Inactive'))
+
+if status == 'Active':
+    st.success('You are active')
+if status == 'Active':
+    st.success('You are active')
